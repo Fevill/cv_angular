625c1b35666cb6126d8ea45143e6fc3f455cfa44
NOUGLOZEH Gerard <nouglozeh_gerard@yahoo.fr>
2018-12-07 18:03:19 +0100

    hhh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { Contact } from './menu/Contact/app.contact';
     Skill
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/skill/app.skill.ts
+++ src/app/menu/skill/app.skill.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Legende } from '../../class/skill';
 
 @Component({
-    selector: 'app-skill',
-    templateUrl: './app.skill.html',
-    styleUrls: ['./app.skill.css']
-  })
-  export class Skill {
+  selector: 'app-skill',
+  templateUrl: './app.skill.html',
+  styleUrls: ['./app.skill.css']
+})
+export class Skill {
 
+  private legendListe: Legende[] = [];
+  private competenceBlocListe: Competence_Bloc[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  test() {
+    this.http.get("./assets/data/skill_text.json").subscribe((data: any) => {
+   
+      for (let element of data.Legende) {
+        this.legendListe.push(new Legende(element.num, element.nom, element.desc))
+      }
+      this.competenceBlocListe = data.skill
+      console.log(this.legendListe)
+      console.log(this.legendListe[1].getNom())
+    });
   }
-  
+}
